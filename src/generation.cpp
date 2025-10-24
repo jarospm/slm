001ea90561c53907cec7c25106b54788b259d18c
Jaros <jaros@chimplie.com>
2025-10-24 18:26:06 +0200

    adjust logic for new k-grams in generation

--- src/generation.cpp
+++ src/generation.cpp
@@ -55,9 +55,22 @@ string generate_text(const map<string, map<char, int>>& kgrams, int length) {
         // Step 4: Update k-gram (remove first char, add sampled char)
         current_kgram = current_kgram.substr(1) + next_char;
         
-        // If new k-gram doesn't exist in model, stop generation
+        // If new k-gram doesn't exist in model...
         if (kgrams.find(current_kgram) == kgrams.end()) {
-            break;
+            // add space and pick a new random kgram
+            output += " ";
+            current_kgram = kgram_list[kgram_dist(gen)];
+            
+            // Check how many characters we can still add
+            int remaining = length - output.length();
+            
+            // Truncate the k-gram if it would exceed the length
+            if (remaining < (int)current_kgram.length()) {
+                output += current_kgram.substr(0, remaining);
+            } else {
+                // Otherwise add space + full k-gram
+                output += current_kgram;
+            }
         }
     }
     
